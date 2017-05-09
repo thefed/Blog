5daec7c96079862516a00906488050177633bfde
thefed <fedch19@gmail.com>
2017-05-09 03:39:43 -0700

    add desc to prob 1

--- 001.twoSum.cpp
+++ 001.twoSum.cpp
@@ -3,6 +3,8 @@
 #include<unordered_map>
 #include<assert.h>
 using namespace std;
+
+// return the index of two numbers, their sum equals to target
 vector<int> twoSum(vector<int>& nums, int target) {
     vector<int> res;
     unordered_map<int, int> m;
@@ -18,20 +20,16 @@ vector<int> twoSum(vector<int>& nums, int target) {
     return res;
 }
 int main() {
-    //int array[] = {12, 2, 2, 12};	// create vector from array
-    //vector<int> nums(array, array + sizeof(array) / sizeof(int));
     vector<int> nums = {12, 2, 2, 12};
     int target = 4;
     vector<int> res = twoSum(nums, target);
     printf("Ret index: %d, %d\n", res[0], res[1]);
     assert(res == (vector<int> {1,2}));
-    //assert(res.size() == 2 && nums[res[0]] + nums[res[1]] == target);
 
     vector<int> nums2 = {12, 2, 4, 12};
     target = 14;
     res = twoSum(nums2, target);
     printf("Ret index: %d, %d\n", res[0], res[1]);
     assert(res == (vector<int> {0,1}));
-    //assert(res.size() == 2 && nums[res[0]] + nums[res[1]] == target);
     return 0;
 }
